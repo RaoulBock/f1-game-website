db9e9fed5bbaae91fe77f0bec21289f350395117
RaoulBock <anthonyraoulbock01@gmail.com>
2023-02-08 12:36:28 +0200

    front-end

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { APP_ICONS } from "../Context/settings";
+import { APP_ICONS, FOOTER_LOGO } from "../Context/settings";
+import logo from "../Components/images/logo.png";
 
 export const Footer = () => {
   return (
     <div className="footer-outline">
       <div className="footer-header-grid">
         <h3>F1</h3>
-        <h3>Social media</h3>
       </div>
-      <div className="footer-header-grid ">
+      <div className="footer-header-grid footer-gap">
         <div className="footer-grid">
           <h4>About F1</h4>
           <h4>News</h4>
@@ -21,6 +21,15 @@ export const Footer = () => {
           <h4>{APP_ICONS.INSTAGRAM}</h4>
           <h4>{APP_ICONS.FACEBOOK}</h4>
         </div>
+      </div>
+      <div className="footer-header-grid">
+        <img src={FOOTER_LOGO} alt={"RP"} className={"footer-logo"} />
+        <img
+          src={
+            "https://media.contentapi.ea.com/content/dam/gin/common/rating-icons/esrb/rating-pending.png"
+          }
+          alt={"RP"}
+        />
       </div>
     </div>
   );
